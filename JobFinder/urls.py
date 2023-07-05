51ab5dbb293a4debd4e282a395206db81aaecfdd
Euclides cardoso <euclidescardoso78@gmail.com>
2023-07-04 23:26:48 -0300

    atualizaçao 1

--- JobFinder/urls.py
+++ JobFinder/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from JobFinder.views import index, cadastro, feed
+from JobFinder.views import index, cadastro, feed,cadastroEmpresa,escolha,batepapo
 
 urlpatterns = [
   path('', index,name='index'),
   path('cadastro/', cadastro, name='cadastro'),
-  path('feed/', feed, name='feed')
+  path('feed/', feed, name='feed'),
+  path('cadastro_empresa',cadastroEmpresa,name='cadastro_empresa'),
+  path('escolha',escolha,name='escolha'),
+  path('batepapo',batepapo,name='batepapo'),
 ]
--- JobFinder/views.py
+++ JobFinder/views.py
@@ -6,5 +6,11 @@ def index(request):
     return render(request, 'telas/index.html')
 def cadastro(request):
     return render(request, 'telas/cadastro.html')
+def cadastroEmpresa(request):
+    return render(request, 'telas/cadastro_empresa.html')
+def escolha(request):
+    return render(request, 'telas/escolha.html')
+def batepapo(request):
+    return render(request, 'telas/batepapo.html')
 def feed(request):
     return render(request,'telas/feed.html')
